4ff348d9fabc102081f38d37e0576cfcb67ab34c
Jason Seo <truth1492@gmail.com>
2021-08-05 02:31:41 -0400

    team budget planner

---
+++ Phase 1/team-budget-planner/program_managers.js
@@ -0,0 +1,39 @@
+function storeData(){
+
+    var client = document.getElementById("clientName").value;
+    var project = document.getElementById("projectName").value;
+    var budget = document.getElementById("budget").value;
+
+    var emp = [ client, project, budget ];
+    localStorage.setItem("empObj",JSON.stringify(emp));
+    console.log("Data stored in local")
+}
+
+function getData(){
+
+    let empObj = localStorage.getItem("empObj");
+    let empJson = JSON.parse(empObj)|| []
+    console.log("emp obj " + empJson);
+}
+
+function removeData(){
+
+    /*<script>
+    window.onload = function() {
+        startcountdown();
+    }   
+    </script> */
+}
+
+function displayData(){
+
+    let empObj = localStorage.getItem("empObj");
+    let empJson = JSON.parse(empObj)
+    var tableContent =""
+    var startTable = "<table border=1 width=200 align=center><tr><th>Id</th><th>Name</th><th>Age</th></tr>"
+    tableContent = "<tr><td>"+empJson[0]+"</td><td>"+empJson[1]+"</td><td>"+empJson[2]+"</td></tr>"
+    var endTable = "</table>"
+    tableContent = startTable+tableContent+endTable
+    document.getElementById("main").innerHTML=tableContent;    
+
+}
