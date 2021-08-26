98baf71eced919d074efe04c63f58eea570ef7a1
Jason Seo <truth1492@gmail.com>
2021-08-25 20:02:13 -0400

    added date&time

--- Phase 3/log-records/readline_sync_values.js
+++ Phase 3/log-records/readline_sync_values.js
@@ -5,13 +5,21 @@ employee.table = [];
 
 let readline = require("readline-sync");
 
+let date_ob = new Date();
+let date = ("0" + date_ob.getDate()).slice(-2);
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+
+let currentDate = (month + "-" + date + " " + hours + ":" + minutes);
+
 for (i=0; i<2; i++){
     let firstname = readline.question("Enter your first name: ");
     let lastname = readline.question("Enter your last name: ");
     let gender = readline.question("Enter your gender: ");
     let emailId = readline.questionEMail("Enter your email id: ");
     
-    employee.table.push({firstname,lastname,gender,emailId});     
+    employee.table.push({firstname,lastname,gender,emailId,currentDate});     
  }
 
  fs.writeFile ("employee.json", JSON.stringify(employee,null,4), function(err) {
@@ -19,3 +27,5 @@ for (i=0; i<2; i++){
      console.log('complete');
      }
  );
+
+ 
